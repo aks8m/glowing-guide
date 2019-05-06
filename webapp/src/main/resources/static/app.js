5c0eca5964fcfc05ac72994922563aa71f13a700
kmaulden <kyle.b.maulden@vanderbilt.edu>
2019-05-06 19:49:50 -0400

    added result list filtering capability

--- webapp/src/main/resources/static/app.js
+++ webapp/src/main/resources/static/app.js
@@ -50,8 +50,8 @@ var compareButton = new Vue({
             axios
                 .get('/api/analysis/compare')
                 .then(response => {
-//                    resultList.results = response.data;
-                    resultList.displayResults(response.data);
+                    resultList.results = response.data;
+//                    resultList.displayResults(response.data);
                  })
                 .catch(error => console.log(error));
 //             resultList.displayResults();
@@ -61,21 +61,50 @@ var compareButton = new Vue({
 
 var resultList = new Vue({
     el: "#resultList",
-    data: { results: null },
-    methods: {
-        displayResults: function(response) {
-            this.results = response;
-            var newsTest = document.getElementById("displayListResults");
-            var itemsTest = response;
-            for(var i = 0; i < itemsTest.length; i++) {
-                var listItem = document.createElement("button");
-                listItem.setAttribute('type', 'button');
-                listItem.setAttribute('class','list-group-item list-group-item-action');
-                listItem.innerHTML = itemsTest[i].output;
-                newsTest.appendChild(listItem);
-               }
+    data: { results: null, all: false, value: false, section: false, attribute: false, ifcountflag: 0, elsecountflag: 0 },
+    computed: {
+        getResults: function() {
+            if (this.all) {
+                return this.results;
+            } else {
+                if (this.results !== null) {
+                    var lis = []
+                    for(var i = 0; i < this.results.length; i++) {
+                        if (this.results[i].resultType == "ATTRIBUTEMISMATCH" && this.attribute) {
+                            lis.push(this.results[i]);
+                        }
+
+                        if (this.results[i].resultType == "VALUEMISMATCH" && this.value) {
+                            lis.push(this.results[i]);
+                        }
+
+                        if (this.results[i].resultType == "SECTIONMISMATCH" && this.section) {
+                            lis.push(this.results[i]);
+                        }
+
+                    }
+                    return lis;
+
+                }
+                return [];
+            }
+
         }
-    }
+    },
+//    methods: {
+//        displayResults: function(response) {
+////            this.results = response;
+//            var newsTest = document.getElementById("displayListResults");
+//            var itemsTest = response;
+//            for(var i = 0; i < itemsTest.length; i++) {
+//                var listItem = document.createElement("button");
+//                listItem.setAttribute('type', 'button');
+//                listItem.setAttribute('class','list-group-item list-group-item-action');
+//                listItem.innerHTML = itemsTest[i].output;
+//                newsTest.appendChild(listItem);
+//               }
+//        },
+//    }
 });
 
 // define the item component
@@ -110,6 +139,17 @@ Vue.component('item', {
         this.open = true
       }
     },
+  }
+});
+
+
+
+//jQuery////////////////////////////////////////////////////////
+$('#all').change(function(e) {
+  if (e.currentTarget.checked) {
+  $('.rows').find('input[type="checkbox"]').prop('checked', true);
+} else {
+    $('.rows').find('input[type="checkbox"]').prop('checked', false);
   }
 });
 
